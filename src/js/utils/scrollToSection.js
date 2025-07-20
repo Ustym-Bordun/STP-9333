ec2439fd83f4c0ab2effc9f363fe2d5f12dc8afe
Ustym Bordun <enderqwer12@gmail.com>
2025-07-20 21:13:17 +0300

    fix/header-paddings-and-add-ids-to-some-sections

--- src/js/utils/scrollToSection.js
+++ src/js/utils/scrollToSection.js
@@ -1,8 +1,30 @@
 import { onClose } from './on-open-or-close-modal-menu.js';
 
+const getHeaderHeight = () => {
+  return window.innerWidth >= 1200 ? 70 : 57;
+};
+
+let headerHeight = getHeaderHeight(); // 70 або 57 при завантаженні
+
+// Оновлюємо змінну при зміні розміру вікна
+window.addEventListener('resize', () => {
+  headerHeight = getHeaderHeight();
+});
+
 const smoothScrollTo = element => {
+  // const offset = headerHeight; // динамічне значення
+  const offset = element.id !== 'hero' ? headerHeight : 0; // динамічне значення
+
+  console.log('Ширина вікна: ', window.innerWidth);
+  console.log('Висота хедеру: ', headerHeight);
+  console.log('Потрібна різниця проскролу: ', offset);
+  console.log('Довжина проскролу: ', element.offsetTop - offset);
+
+  console.dir(element);
+  console.log('Ід елементу:', element.id);
+
   window.scrollTo({
-    top: element.offsetTop,
+    top: element.offsetTop - offset,
     behavior: 'smooth',
   });
 };
@@ -15,8 +37,9 @@ export const scrollToSection = element => {
 
     const targetElement = document.getElementById(targetId);
 
-    // console.log(targetId);
-    // console.log(targetElement);
+    console.log(targetId);
+    console.dir(targetElement);
+    console.log(targetElement);
 
     if (targetElement) {
       // console.log(1);
